1b0d2e38a2be0421ede82b632f5a55fadb892eab
TheCodingLand <julien.lebourg8@gmail.com>
2024-10-30 10:32:04 +0100

    fix tests

--- tests/test_configmap.py
+++ tests/test_configmap.py
@@ -1,2 +1,37 @@
-def test_configmap():
-    assert True
+from pathlib import Path
+from typing import List, TypeVar
+
+from pydantic import BaseModel, Field
+
+from actions import get_all_defined_game_actions
+from configmap import ActionMapsFile, get_action_maps_file, Action
+
+
+SC = "LIVE"
+SC_VERSION = "sc-alpha-3.24.2-9381373"
+sc_actionmaps_path = Path(__file__).parent / 'data' / SC_VERSION / f"actionmap.json"
+
+def test_actionmap():
+        
+    possible_actions = get_all_defined_game_actions()
+
+
+    x = get_action_maps_file()
+    name_action_mapping = { action.name: action for action in possible_actions.values()}
+    x = ActionMapsFile(**x)
+
+    TItem = TypeVar("TItem")
+
+    def always_list(item: TItem | List[TItem]) -> List[TItem]:
+        if isinstance(item, list):
+            return item #type: ignore
+        return [item]
+
+    for actions in x.action_profiles[0].actionmap:
+        for action in actions.action:
+            for item in always_list(action):
+                if isinstance(action, Action):
+                    if action.rebinding is not None:
+                        pass
+                            
+                            # defined keys
